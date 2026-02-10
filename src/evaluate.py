d7ec8b0b91446f83517f11b5b5ebef7baf1a628d
ribond007 <rishabh.b1704@gmail.com>
2026-02-10 21:40:21 +0530

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,1 +1,21 @@
+import joblib
+from datasets import load_dataset
+from sklearn.metrics import classification_report
+
+HF_USERNAME = "RishiBond"
+PROCESSED_DATASET = "engine-predictive-maintenance-processed"
+
+def evaluate():
+    model = joblib.load("artifacts/model.pkl")
+    test_ds = load_dataset(f"{HF_USERNAME}/{PROCESSED_DATASET}", split="test")
+    df = test_ds.to_pandas()
+
+    X = df.drop(columns=["engine_condition"])
+    y = df["engine_condition"]
+
+    preds = model.predict(X)
+    print(classification_report(y, preds))
+
+if __name__ == "__main__":
+    evaluate()
 
