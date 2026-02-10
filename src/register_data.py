2e45a0ce153c82aedeeb4e9ab7ecb1c505ab8146
ribond007 <rishabh.b1704@gmail.com>
2026-02-10 21:32:38 +0530

    Update register_data.py

--- src/register_data.py
+++ src/register_data.py
@@ -1,1 +1,19 @@
+from datasets import load_dataset
+from huggingface_hub import HfApi
+import os
+
+DATASET_NAME = "engine-predictive-maintenance"
+HF_USERNAME = "ribond007"   # change if needed
+
+def register_dataset():
+    dataset = load_dataset(
+        "csv",
+        data_files="data/raw/engine_data.csv"
+    )
+
+    dataset.push_to_hub(f"{HF_USERNAME}/{DATASET_NAME}")
+    print("Dataset successfully pushed to Hugging Face Hub")
+
+if __name__ == "__main__":
+    register_dataset()
 
